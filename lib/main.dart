3d9c1802c198e408408a1a44162d02824f137bba
Hassan Teslim <hassanteslim12@gmail.com>
2025-06-07 19:35:29 +0100

    simplify main widget structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,10 @@
-import 'package:animations_lesson/challenge.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: HeroFirstPage()));
-
-//Hero animations automatically animate a widget from one screen to another when it's shared between routes using a Hero widget with the same tag.
-
-class HeroFirstPage extends StatelessWidget {
-  const HeroFirstPage({super.key});
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Hero Animation - Page 1"),
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => HeroChallengeA()),
-              );
-            },
-            icon: Icon(Icons.question_mark),
-          ),
-        ],
-      ),
-      body: Center(
-        child: GestureDetector(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (_) => HeroSecondPage()),
-            );
-          },
-          child: Hero(
-            tag: 'heroBox',
-            child: Container(width: 100, height: 100, color: Colors.purple),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class HeroSecondPage extends StatelessWidget {
-  const HeroSecondPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Hero Animation - Page 2")),
-      body: Center(
-        child: Hero(
-          tag: 'heroBox',
-          child: Container(width: 300, height: 300, color: Colors.red),
-        ),
-      ),
-    );
+    return Scaffold(body: Center(child: Text('Animations')));
   }
 }
